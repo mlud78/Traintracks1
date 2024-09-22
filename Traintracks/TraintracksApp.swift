bc50e2aa5b9994b2e8128d3a596962d81edfb16b
poopburger78 <poopburger78@gmail.com>
2024-09-22 16:34:19 -0700

    Initial Commit

---
+++ Traintracks/TraintracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraintracksApp.swift
+//  Traintracks
+//
+//  Created by Maddy Ludes on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TraintracksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TraintracksTests/TraintracksTests.swift
@@ -0,0 +1,16 @@
+//
+//  TraintracksTests.swift
+//  TraintracksTests
+//
+//  Created by Maddy Ludes on 9/22/24.
+//
+
+import Testing
+
+struct TraintracksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
